e466f807fb95c58a33e304871999798dd6f4744b
Marcia <msdfcarvalho14@gmail.com>
2023-02-21 13:18:10 -0500

    fixed the nav bar

--- src/script.js
+++ src/script.js
@@ -1,3 +1,36 @@
+/* Progress bar Effect */
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+
+window.addEventListener("scroll", () => {
+  mainFn();
+});
+
+const mainFn = () => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
+      });
+      navbarLinks[i].classList.add("change");
+    }
+  });
+};
+
+mainFn();
+
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
+
 /* Typing and Deleting Effect */
 // List of sentences
 var _CONTENT = ["FRONT-END DEVELOPER", "DESIGNER", "FREELANCER"];
